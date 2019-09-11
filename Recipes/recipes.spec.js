624f0506d72c0b1f59aa1f17ca9e9a10c47a9a31
fritzgt <tuanis@live.com>
2019-09-11 18:04:54 +0300

    created first model test for recipes

--- Recipes/recipes.spec.js
+++ Recipes/recipes.spec.js
@@ -1,3 +1,27 @@
+// const request = require('supertest');
+
 const db = require('../data/db-config');
 
-const recipes = require('./recipesModel');
+const Recipes = require('./recipesModel');
+
+describe('recipes model', () => {
+  describe('insert()', () => {
+    //delete current entries to start test fresh
+    beforeEach(async () => {
+      await db('recipes').truncate();
+    });
+    //check if we can insert new entries
+    it('it should create two new posts', async () => {
+      await Recipes.addRecipe({
+        title: 'Go to the GYM',
+        meal_type: 'Lunch',
+        chef_id: 6,
+        recipe_img: 'nijewnce',
+        ingredients: 'many',
+        instructions: 'cook'
+      });
+      const todo = await db('recipes');
+      expect(todo).toHaveLength(1);
+    });
+  });
+});
